f962f2149dd2a2693c2656585fa6c19fdd387bed
Mikusch <mikuschger@gmail.com>
2020-08-31 10:49:23 +0200

    Second attempt at fixing package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,8 +10,8 @@ mkdir -p package/sound
 
 # Copy all required files to package
 cp -r addons/sourcemod/plugins/tfgo.smx package/addons/sourcemod/plugins
-cp -r ../configs/tfgo package/addons/sourcemod/configs
-cp -r ../gamedata/tfgo.txt package/addons/sourcemod/gamedata
-cp -r ../translations package/addons/sourcemod
+cp -r ../addons/sourcemod/configs/tfgo package/addons/sourcemod/configs
+cp -r ../addons/sourcemod/gamedata/tfgo.txt package/addons/sourcemod/gamedata
+cp -r ../addons/sourcemod/translations package/addons/sourcemod
 cp -r ../sound package
 cp -r ../LICENSE package
